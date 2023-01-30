2dccbc3ae3ec80ea74f0d12b5f0beafb55ff6afc
Anup <anupchaudhary066@gmail.com>
2023-01-30 15:46:41 +0545

    update

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", this.window.scrollY > 0);
 });
 
 let menu = document.querySelector("#menu-icon");
